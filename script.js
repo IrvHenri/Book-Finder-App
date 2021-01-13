08232e11e924ce193b94b87bbe6dda6142a962a3
IrvHenri <69181038+IrvHenri@users.noreply.github.com>
2021-01-12 21:30:14 -0500

    refactored code and improved book styling

--- script.js
+++ script.js
@@ -1,49 +1,62 @@
-let msgWaiting = document.querySelector(".message-waiting");
-let searchForm = document.getElementById("search-form");
-
+const placeHolderMessage = document.querySelector(".place-holder-message");
+const searchForm = document.getElementById("search-form");
 const booksDisplay = document.getElementById("books-display");
-
-let getGoogleBooks = "https://www.googleapis.com/books/v1/volumes?q=";
-
+const defaultBookImage =
+  "https://www.flaticon.com/svg/vstatic/svg/879/879891.svg?token=exp=1610492090~hmac=5ea365a887b3732e935d0ff29cd15eaf";
+const googleBooksURL = "https://www.googleapis.com/books/v1/volumes?q=";
 let apiKey = "&key=AIzaSyCw6aeDwd2srAlaiVBFSSBvyl2O7atCTaA";
 
 searchForm.addEventListener("submit", () => {
-  msgWaiting.style.display = "none";
-  let searchInput = document.getElementById("searchInput").value;
-  let searchResult = `${getGoogleBooks}${searchInput}${apiKey}`;
+  placeHolderMessage.style.display = "none";
+  let searchInput = document.getElementById("search-input").value;
+  let searchQuery = `${googleBooksURL}${searchInput}${apiKey}`;
 
-  async function getBooks() {
-    const booksPromise = await fetch(searchResult);
-    const getBook = await booksPromise.json();
-    booksDisplay.innerHTML = "";
-    getBook.items.map((book) => {
-      console.log(book.volumeInfo);
-
-      let title = book.volumeInfo.title;
-      let authors = book.volumeInfo.authors;
-      let previewLink = book.volumeInfo.previewLink;
-      let publishdate = book.volumeInfo.publishedDate;
-      let publisher = book.volumeInfo.publisher;
-      let smallThumbnail = book.volumeInfo.imageLinks.smallThumbnail;
-
-      let div = document.createElement("div");
-      div.innerHTML = `
-      
-      <h3>  ${title} </h3>
-      <div class= "book-img">
-       <img src= "${smallThumbnail}" alt= "Image book cover "> 
-       </div>
-        <div class = "book-details">
-        <small> <strong>Author(s)</strong>: ${authors}</small>
-        <small> <strong>Publishers</strong>: ${publisher}</small>
-        <small> <strong>Published</strong>: ${publishdate}</small>
-        </div>
-        <a href = "${previewLink}" class ="book-link" target="_blank">  Google Books Preview </a>
-         `;
-      div.classList.add("book-styling");
-
-      booksDisplay.append(div);
-    });
-  }
-  getBooks();
+  getBooks(searchQuery);
 });
+
+async function getBooks(query) {
+  const booksPromise = await fetch(query);
+  const booksData = await booksPromise.json();
+
+  if (booksData.items) {
+    displayBooks(booksData.items);
+  } else {
+    booksDisplay.innerHTML = "";
+    placeHolderMessage.style.display = "block";
+    placeHolderMessage.innerHTML = "Sorry no results!";
+  }
+}
+
+function displayBooks(books) {
+  booksDisplay.innerHTML = "";
+  books.forEach((book) => {
+    let volumeInfo = book.volumeInfo;
+    let bookImage = volumeInfo.imageLinks;
+    let title = volumeInfo.title || "N/A";
+    let authors = volumeInfo.authors || "N/A";
+    let previewLink = volumeInfo.previewLink || "N/A";
+    let publishdate = volumeInfo.publishedDate || "N/A";
+    let publisher = volumeInfo.publisher || "N/A";
+    let smallThumbnail =
+      bookImage && bookImage.smallThumbnail
+        ? bookImage.smallThumbnail
+        : defaultBookImage;
+
+    let div = document.createElement("div");
+    div.innerHTML = `
+    
+    <h3>  ${title} </h3>
+    <div class= "book-img">
+     <img src= "${smallThumbnail}" alt= "Image book cover "> 
+     </div>
+      <div class = "book-details">
+      <small> <strong>Author(s)</strong>: ${authors}</small>
+      <small> <strong>Publishers</strong>: ${publisher}</small>
+      <small> <strong>Published</strong>: ${publishdate}</small>
+      </div>
+      <a href = "${previewLink}" class ="book-link" target="_blank">  Google Books Preview </a>
+       `;
+    div.classList.add("book-styling");
+    booksDisplay.append(div);
+  });
+}
